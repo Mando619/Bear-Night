a2fdadc851effe36a57c807fbc409d9dbd3802f3
Mando Estrada <libryum33@hotmail.com>
2022-06-23 18:48:48 -0700

    added navbar

--- bear-night/src/Components/Footer.js
+++ bear-night/src/Components/Footer.js
@@ -1,15 +1,46 @@
 import React from 'react';
 
 
-
+// changed to react icons later. Get from portfolio.
 
 
 const Footer = () => {
     return (
         <div>
 
-        </div>
-    )
+<div className="container my-5">
+
+    <footer className="text-center text-white">
+
+<div className= "container pt-4">
+    <section className="mb-4">
+    <a
+        class="btn btn-link btn-floating btn-lg text-dark m-1"
+        href="#!"
+        role="button"
+        data-mdb-ripple-color="dark"
+        ><i class="fab fa-facebook-f"></i
+      ></a>
+      <a
+        class="btn btn-link btn-floating btn-lg text-dark m-1"
+        href="#!"
+        role="button"
+        data-mdb-ripple-color="dark"
+        ><i class="fab fa-Instagram"></i
+      ></a>
+    </section>
+    <div className="text-center text-dark p-3"><i class="fa fa-copyright" aria-hidden="true"></i>2022 Copyright:</div>
+</div>
+
+    </footer>
+
+
+
+
+</div>
+
+</div>
+    )     
 }
 
 export default Footer;
